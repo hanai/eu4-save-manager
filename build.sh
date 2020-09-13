1ebf6325954466f957b65b32bce846885338d8c6
Hanai <ihanai1991@gmail.com>
2020-09-14 00:53:57 +0800

    add exclude-module

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONOPTIMIZE=2 pyinstaller -y -w --clean --log-level=WARN -n EU4SaveManager -F eu4-save-manager.py
+PYTHONOPTIMIZE=2 pyinstaller -y -w --clean --log-level=WARN -n EU4SaveManager -F --exclude-module email --exclude-module urllib.request --exclude-module unittest --exclude-module pdb --exclude-module PySide2.QtNetwork eu4-save-manager.py
